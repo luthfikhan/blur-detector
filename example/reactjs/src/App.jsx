bea2fd1fd1bd643c90172887609b2cedae43bea8
Luthfi Khoirul Anwar <luthfikhoirulanwar55@gmail.com>
2022-07-26 13:05:24 +0700

    Example for reactjs

---
+++ example/reactjs/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import blurDetector from "blur-detector";
+import "./App.css";
+
+function App() {
+  const [image, setImage] = useState(null);
+  const [blurScore, setBlurScore] = useState("");
+
+  const toBase64 = (fileToConvert) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(fileToConvert);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = (error) => reject(error);
+    });
+
+  const onFileChange = async (e) => {
+    const imageBase64 = await toBase64(e.target.files[0]);
+    setImage(imageBase64);
+
+    // const result = await blurDetector(imageBase64);
+    const result = await blurDetector(e.target.files[0]);
+    setBlurScore(result.blurScore);
+  };
+
+  return (
+    <div className="App">
+      <h1>Blur Detector Demo</h1>
+      <p>
+        Select an image to measure its blur. It will only be processed in your
+        browser, not uploaded anywhere.
+      </p>
+      <input onChange={onFileChange} type="file" accept="image/*" />
+      {image && (
+        <div>
+          <div style={{ padding: "20px 0", color: "aquamarine" }}>
+            <div style={{ fontSize: "2rem", marginBottom: "4px" }}>
+              Score: {blurScore}
+            </div>
+            <div>Higher score means more blur.</div>
+          </div>
+          <img src={image} alt="image" />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
