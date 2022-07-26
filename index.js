266ccb76b3ecd44a2e7b50fccf46569cb88e532e
Luthfi Khoirul Anwar <luthfikhoirulanwar55@gmail.com>
2022-07-26 12:34:46 +0700

    add File support

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-const measureBlur = require("./measureBlur");
+const measureBlur = require("./src/measureBlur");
 const canvasModule = require("canvas");
 
 const { createCanvas, loadImage } = canvasModule;
+
+const toBase64 = (fileToConvert) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(fileToConvert);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+
 /**
- * @param  {String|Buffer} src
+ * @param  {String|Buffer|File} src
  */
 const blurDetector = async (src) => {
+  if (src instanceof File) {
+    src = await toBase64(src);
+  }
+
   const image = await loadImage(src);
   const canvas = createCanvas(image.width, image.height);
   const context = canvas.getContext("2d");
